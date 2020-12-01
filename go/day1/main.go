37112c3f4c3d5407fbf6851ad4f52ad6326b7346
jcockbain <jcockbain96@gmail.com>
2020-12-01 09:05:03 +0000

    add more go tests for day 1

--- go/day1/main.go
+++ go/day1/main.go
@@ -1,47 +1,50 @@
 package main
 
-import (	
+import (
 	input "aoc2020/inpututils"
 
-	"fmt"
 	"errors"
+	"fmt"
 )
 
-func main(){
+func main() {
 	fmt.Println("--- Part One ---")
 	fmt.Println(Part1("input.txt"))
-	
+
 	fmt.Println("--- Part Two ---")
 	fmt.Println(Part2("input.txt"))
 }
 
+// create set type as Go doesn't have one
 type set map[int]struct{}
 
-func (s set) add(n int){
+func (s set) add(n int) {
 	s[n] = struct{}{}
 }
 
-func (s set) has(n int) bool{
+func (s set) has(n int) bool {
 	_, ok := s[n]
 	return ok
 }
 
 func Part1(filename string) int {
 	nums := input.ReadNumbers(filename)
-	multiplied, err := twoSum(nums, 2020)
+
+	product, err := twoSum(nums, 2020)
 	if err != nil {
 		return -1
 	}
-	return multiplied
+	return product
 }
 
 func Part2(filename string) int {
 	nums := input.ReadNumbers(filename)
-	for i := 0; i < len(nums) - 2; i += 1 {
+
+	for i := 0; i < len(nums)-2; i += 1 {
 		num := nums[i]
-		multiplier, err := twoSum(nums[i+1:], 2020 - num)
+		product, err := twoSum(nums[i+1:], 2020-num)
 		if err == nil {
-			return multiplier * num
+			return product * num
 		}
 	}
 	return -1
@@ -49,17 +52,11 @@ func Part2(filename string) int {
 
 func twoSum(ints []int, target int) (int, error) {
 	seen := set{}
-	for _, i := range(ints){
-		if seen.has(target - i){
+	for _, i := range ints {
+		if seen.has(target - i) {
 			return (target - i) * i, nil
 		}
 		seen.add(i)
 	}
 	return -1, errors.New("No solution")
-}
-
-func check(err error) {
-	if err != nil {
-		panic(err)	
-	}
 }
--- go/day1/main_test.go
+++ go/day1/main_test.go
@@ -1,16 +1,55 @@
 package main
 
 import (
-	"testing"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestPart1(t *testing.T) {
 	got := Part1("input.txt")
-	assert.Equal(t, 440979 , got)
+	assert.Equal(t, 440979, got)
 }
 
 func TestPart2(t *testing.T) {
 	got := Part2("input.txt")
 	assert.Equal(t, 82498112, got)
+}
+
+func testTwoSum(t *testing.T){
+	t.Parallel()
+	testCases := []struct{
+		name string
+		ints []int
+		target int
+		expected int
+	} {
+		{
+			"test-1",
+			[]int{1, 2, 3, 4 },
+			5,
+			6,
+		},
+		{
+			"test-2",
+			[]int{10, 21, 31, 4 },
+			14,
+			40,
+		},
+		{
+			"test-3",
+			[]int{12, 1, 23, 3},
+			4,
+			3,
+		},
+	}
+
+	for _, tt := range testCases {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T){
+			t.Parallel()
+			got, err := twoSum(tt.ints, tt.target)
+			assert.Nil(t, err)
+			assert.Equal(t, tt.expected, got)
+		})
+	}
 }
