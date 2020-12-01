b478cff0005694936572b321ba48d198f3085c24
jcockbain <jcockbain96@gmail.com>
2020-12-01 17:09:38 +0000

    add instructions to new_day.sh

--- helpers/new_day.sh
+++ helpers/new_day.sh
@@ -23,6 +23,10 @@ cp -n -TR go/template go/day${DAY}
 INPUT_URL="https://adventofcode.com/${YEAR}/day/${DAY}/input"
 TEMP_INPUT="temp-input.txt"
 
+# "session cookie" must be given as input is per-user...
+# this can be grabbed from the URL of any input page online e.g https://adventofcode.com/2019/day/2/input
+# I have this set as an environment variable here
+
 curl "${INPUT_URL}" -H "cookie: session=${AOC_COOKIE}" -o "${TEMP_INPUT}" 2>/dev/null
 
 cp ${TEMP_INPUT} ${GOROOT}/input.txt
