188a4a390dfd1f8d84d0abc98bf3685769ebe85f
jcockbain <jcockbain96@gmail.com>
2020-12-03 18:43:03 +0000

    simpler day 3 go

--- go/day3/main.go
+++ go/day3/main.go
@@ -19,6 +19,14 @@ type treeCounter struct {
 	h, w int
 }
 
+func newTreeCounter(lines []string) treeCounter {
+	return treeCounter {
+		lines: lines,
+		w: len(lines[0]),
+		h: len(lines),
+	}
+} 
+
 func (tc treeCounter) count (dx int, dy int) int {
 	x, y, trees := 0, 0, 0
 	for y < tc.h {
@@ -33,20 +41,12 @@ func (tc treeCounter) count (dx int, dy int) int {
 
 func Part1(filename string) int {
 	lines := input.ReadLines(filename)
-	tc := treeCounter {
-		lines: lines,
-		w: len(lines[0]),
-		h: len(lines),
-	}
+	tc := newTreeCounter(lines)
 	return tc.count(3, 1)
 }
 
 func Part2(filename string) int {
 	lines := input.ReadLines(filename)
-	tc := treeCounter {
-		lines: lines,
-		w: len(lines[0]),
-		h: len(lines),
-	}
+	tc := newTreeCounter(lines)
 	return tc.count(1, 1) * tc.count(3, 1) * tc.count(5, 1) * tc.count(7, 1) * tc.count(1, 2)
 }
