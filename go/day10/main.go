2f120d4e56dfaca3ca64cc819baab64f276e9b13
jcockbain <jcockbain96@gmail.com>
2020-12-10 06:56:12 +0000

    add day 10 go solution

--- go/day10/main.go
+++ go/day10/main.go
@@ -4,7 +4,7 @@ import (
 	input "aoc2020/inpututils"
 
 	"fmt"
-	"strconv"
+	"sort"
 )
 
 func main() {
@@ -17,25 +17,62 @@ func main() {
 
 func Part1(filename string) int {
 	nums := input.ReadNumbers(filename)
-	sum := 0
-	for _, i := range nums {
-		sum += i
+	sort.Ints(nums)
+
+	ones, threes, v := 0, 0, 0
+
+	for _, x := range nums {
+		if (x - v) == 1 {
+			ones += 1
+		} else if (x - v) == 3 {
+			threes += 1
+		}
+		v = x
 	}
-	return sum
+	return ones * (threes + 1)
 }
 
 func Part2(filename string) int {
-	return 12
-}
+	nums := input.ReadNumbers(filename)
 
-func check(err error) {
-	if err != nil {
-		panic(err)
+	numSet := map[int]struct{}{}
+	for _, n := range nums {
+		numSet[n] = struct{}{}
 	}
+
+	maxV := max(nums)
+	cache := make([]int, maxV)
+	for i := range cache {
+		cache[i] = -1
+	}
+
+	var numberOfWays func(int) int
+
+	numberOfWays = func(v int) int {
+		if _, ok := numSet[v]; !ok {
+			return 0
+		}
+		if v == maxV {
+			return 1
+		}
+		if cache[v] == -1 {
+			s := 0
+			for _, x := range []int{1, 2, 3} {
+				s += numberOfWays(v + x)
+			}
+			cache[v] = s
+		}
+		return cache[v]
+	}
+	return numberOfWays(1) + numberOfWays(2) + numberOfWays(3)
 }
 
-func toInt(s string) int {
-	i, err := strconv.Atoi(s)
-	check(err)
-	return i
+func max(s []int) int {
+	m := s[0]
+	for _, v := range s {
+		if v > m {
+			m = v
+		}
+	}
+	return m
 }
--- go/day10/main_test.go
+++ go/day10/main_test.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestPart1(t *testing.T) {
-	got := Part1("input.txt")
-	assert.Equal(t, 15, got)
+	assert.Equal(t, 35, Part1("test1.txt"))
+	assert.Equal(t, 220, Part1("test2.txt"))
+	assert.Equal(t, 1856, Part1("input.txt"))
 }
 
 func TestPart2(t *testing.T) {
-	got := Part2("input.txt")
-	assert.Equal(t, 12, got)
+	assert.Equal(t, 8, Part2("test1.txt"))
+	assert.Equal(t, 19208, Part2("test2.txt"))
+	assert.Equal(t, 2314037239808, Part2("input.txt"))
 }
